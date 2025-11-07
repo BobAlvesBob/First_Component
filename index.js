f5fe9fe372ca85f96938b192758f6cea1f8e7575
Bob <bobpitaia@gmail.com>
2025-11-07 14:46:12 +0000

    ternary operator v01

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
- function getNum() {
+function getNum() {
   return Math.floor(Math.random() * 10) + 1;
 }
 
 class JSXDemo extends React.Component {
   render() {
     const num = getNum();
+    let msg;
+     msg =(num === 7) ? 
+       (
+        <div>
+          Congratulations You Win!
+          <br />
+          <img src="yes.gif" alt="Celebration" />
+        </div>
+      )
+   :
+       (
+        <div>
+          Sorry You Lose!
+          <br />
+          <img src="cry-498-x-373-gif-gqa0f76s1a2k54bt.gif" alt="Sad" />
+        </div>
+      )
+    
     return (
       <div>
-        <h1>Your lucky number is: {num}</h1>
-        
-          {num === 7  ? (
-            <div>
-            Congratulations You Win!<br />
-            <img src="yes.gif" alt="Celebration" />
-            </div>
-
-  ) : ( 
-    <div>Sorry You Lose!<br />
-    <img src="cry-498-x-373-gif-gqa0f76s1a2k54bt.gif" alt="Sad" />
-
-    </div>)
-  }
-          </div>
+        <h1> Your number is:{num}</h1>
+        {msg}
+      </div>
     );
   }
 }
