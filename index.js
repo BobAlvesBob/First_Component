08a097b56c293b555c5118dbd3038734d1b46b7d
Bob <bobpitaia@gmail.com>
2025-11-07 13:35:01 +0000

    number picker game v02

--- index.js
+++ index.js
@@ -8,15 +8,19 @@ class JSXDemo extends React.Component {
     return (
       <div>
         <h1>Your lucky number is: {num}</h1>
-        <p></p>
-          {num === 7 ? (
+        
+          {num === 7  ? (
             <div>
             Congratulations You Win!<br />
             <img src="yes.gif" alt="Celebration" />
             </div>
-          ) : (
-            <div>Sorry You Lose!</div>
-          )}
+
+  ) : ( 
+    <div>Sorry You Lose!<br />
+    <img src="cry-498-x-373-gif-gqa0f76s1a2k54bt.gif" alt="Sad" />
+
+    </div>)
+  }
           </div>
     );
   }
