54b8afd0fcb4fa75abb3b80416bb444fd4112ef1
Bob <bobpitaia@gmail.com>
2025-11-07 12:10:50 +0000

    JSX - random mood generator

---
+++ index.js
@@ -0,0 +1,17 @@
+function getMood(){
+  const moods = ['Happy', 'Sad', 'Anxious', ' Euphoric'];
+  return moods[Math.floor(Math.random() * moods.length)];
+}
+
+class JSXDemo extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>My current mood is: {getMood()} </h1> 
+     
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<JSXDemo />, document.getElementById("root"));
